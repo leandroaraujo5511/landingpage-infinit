22d6db35fbac3a5af31e21288ecbd29881d2e5fc
Leandro Goncalves <leandroaraujo5511@gmail.com>
2026-04-22 18:21:02 -0300

    Refactor Vite configuration to include additional allowed hosts for preview

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  preview: {
-    host: '0.0.0.0',
-    port: Number(process.env.PORT) || 4173,
-    strictPort: Boolean(process.env.PORT),
-    allowedHosts: ['.easypanel.host', 'apps-landingpage-infinit-tech.doqkcg.easypanel.host'],
-  },
-})
+	plugins: [react()],
+	preview: {
+		host: "0.0.0.0",
+		port: Number(process.env.PORT) || 4173,
+		strictPort: Boolean(process.env.PORT),
+		allowedHosts: [
+			"infinittech.com.br",
+			"www.infinittech.com.br",
+			".easypanel.host",
+		],
+	},
+});
