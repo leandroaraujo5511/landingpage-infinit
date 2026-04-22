fa2557a643f6ac315a068431f166729156cb486d
Leandro Goncalves <leandroaraujo5511@gmail.com>
2026-04-22 18:04:24 -0300

    Update Vite configuration to allow specific hosts for development

--- vite.config.ts
+++ vite.config.ts
@@ -8,5 +8,6 @@ export default defineConfig({
     host: '0.0.0.0',
     port: Number(process.env.PORT) || 4173,
     strictPort: Boolean(process.env.PORT),
+    allowedHosts: ['.easypanel.host', 'apps-landingpage-infinit-tech.doqkcg.easypanel.host'],
   },
 })
